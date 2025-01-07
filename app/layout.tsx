cdcefa2c08fa02f1dd253fd3a05a34c3ddf1eba0
Iskandar Kambaraliyev <human.github@gmail.com>
2025-01-08 01:54:27 +0500

    add meta icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,6 +62,46 @@ export const metadata: Metadata = {
       },
     ],
   },
+  metadataBase: new URL("https://interno.stiv.uz"),
+  alternates: {
+    canonical: "/",
+  },
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+    other: [
+      {
+        url: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        rel: "icon",
+      },
+      {
+        url: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        rel: "icon",
+      },
+      {
+        url: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+        rel: "apple-touch-icon",
+      },
+      {
+        url: "/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+        rel: "icon",
+      },
+      {
+        url: "/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+        rel: "icon",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
